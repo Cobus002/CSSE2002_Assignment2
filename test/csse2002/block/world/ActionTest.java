377059dc540ac31d994e4478ab7ed5b234fad56e
Cobus Hoffmann <hoffmann.cobus@hotmail.com>
2018-09-14 11:04:18 +1000

    test folder added

---
+++ test/csse2002/block/world/ActionTest.java
@@ -0,0 +1,20 @@
+package csse2002.block.world;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ActionTest {
+
+    @Test
+    public void getPrimaryAction() {
+    }
+
+    @Test
+    public void getSecondaryAction() {
+    }
+
+    @Test
+    public void loadAction() {
+    }
+}
