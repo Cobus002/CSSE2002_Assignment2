48f4290d3e6bfc17271eba07dc79f48cfe935708
Cobus <hoffmann.cobus@icloud.com>
2018-09-29 19:42:37 +1000

    Added the main class for the application

---
+++ src/csse2002/block/world/Main.java
@@ -0,0 +1,15 @@
+package csse2002.block.world;
+
+public class Main {
+
+    /**
+     * Main() function is the entry point of the application and takes 3
+     * arguments, input map file args[0] actions file args[1] and an output
+     * map file args[2].
+     * @param args
+     */
+    public static void main(String args[]){
+        //Todo: implement function
+
+    }
+}
